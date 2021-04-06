19b64e27486525185babc281edd19890569d47d8
Ulya <example@gmail.com>
2021-04-06 15:21:51 +0300

    Added isOpened for all elements

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const width = 400;
 const mineX = [];
 const mineY = [];
 let counts = [];
+let isOpened = [];
 
 ctx.fillStyle = 'rgb(200, 250, 240)';
 ctx.fillRect(0, 0, width, height);
@@ -34,11 +35,11 @@ function countAround(i, j) {
     for (let a = i - 1; a < i + 2; a++) {
         for (let b = j - 1; b < j + 2; b++) {
 
-            for (let l=0; l<10; l++){
-            if (mineX[l] == a && mineY[l] == b) {
-                count++;
+            for (let l = 0; l < 10; l++) {
+                if (mineX[l] == a && mineY[l] == b) {
+                    count++;
+                }
             }
-        }
         }
     }
     return count;
@@ -54,6 +55,7 @@ for (let i = 0; i < 10; i++) { // объявление мин
 
 for (let i = 0; i < 10; i++) {
     counts[i] = [];
+    isOpened[i]=[];
     for (let j = 0; j < 10; j++) {
         //let count = 0;
         // for (let a = i - 1; a < i + 2; a++) {
@@ -64,6 +66,7 @@ for (let i = 0; i < 10; i++) {
         //         }
         //     }
         counts[i][j] = countAround(i, j);
+        isOpened[i][j] = false;
         ctx.textBaseline = "top";
         ///ctx.fillText(`${counts[i][j]}`, i * grid, j * grid);
     }
@@ -97,20 +100,26 @@ canvas.addEventListener('click', function (evt) {
 
             // это строка - работает 
             //ctx.fillText(`${countAround(userX, userY)}`, userX * grid, userY * grid);
-
+isOpened[userX][userY]=true;
             ctx.fillText(`${counts[userX][userY]}`, userX * grid, userY * grid);
 
-            // for (let a = userX - 1; a < userX + 2; a++) {
-            //     for (let b = userY - 1; b < userY + 2; b++) {
-            //         if (mineX[i] == a && mineY[i] == b) {
-            //             count++;
-            //         }
-            //         console.log(a, b, mineX[i], mineY[i]);
-            //     }
-            // }
-           // console.log(count);
-            
-
+            if (counts[userX][userY] == 0) {
+                for (let c = userX - 1; c < userX + 2; c++) {
+                    for (let d = userY - 1; d < userY + 2; d++) {
+                        if (c >= 0 && d >= 0 && counts[c][d] == 0) {
+                            for (let e = c - 1; e < c + 2; e++) {
+                                for (let f = d - 1; f < d + 2; f++) {
+                                    if (e >= 0 && f >= 0) {
+                                        ctx.fillText(`${counts[e][f]}`, e * grid, f * grid);
+                                    }
+                                }
+                            }
+                        ctx.fillText(`${counts[c][d]}`, c * grid, d * grid);    
+                        }
+                        
+                    }
+                }
+            }
 
         }
 
